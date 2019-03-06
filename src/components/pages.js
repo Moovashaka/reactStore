34c59c7abc75c26d67d3f10eca634962940b808c
Moova Shaka <mrmoovashaka@gmail.com>
2019-03-06 22:22:39 +0000

    React React Router Store

---
+++ src/components/pages.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Pages extends Component {
+  render () {
+    return (
+      <div>
+      <h3>Hello from Pages</h3>
+      </div>
+    )
+  }
+}
